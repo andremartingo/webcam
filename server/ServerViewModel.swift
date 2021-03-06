df21806e57b83b824b631cf282871b331fd5ebc0
Andre Martingo <andre.martingo@gmail.com>
2021-03-06 22:21:43 +0000

    Rotate Image

--- server/ServerViewModel.swift
+++ server/ServerViewModel.swift
@@ -31,8 +31,32 @@ class ServerViewModel: ObservableObject {
             guard let image = UIImage(data: $0) else { return }
             DispatchQueue.main.async {
                 self.connectionState = .connected
-                self.image = image
+                self.image = image.rotate(deg: 90)
             }
         }
     }
+}
+
+extension UIImage {
+    func rotate(deg degrees: CGFloat) -> UIImage {
+        //Calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox: UIView = UIView(frame: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        let t: CGAffineTransform = CGAffineTransform(rotationAngle: degrees * CGFloat.pi / 180)
+        rotatedViewBox.transform = t
+        let rotatedSize: CGSize = rotatedViewBox.frame.size
+        //Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap: CGContext = UIGraphicsGetCurrentContext()!
+        //Move the origin to the middle of the image so we will rotate and scale around the center.
+        bitmap.translateBy(x: rotatedSize.width / 2, y: rotatedSize.height / 2)
+        //Rotate the image context
+        bitmap.rotate(by: (degrees * CGFloat.pi / 180))
+        //Now, draw the rotated/scaled image into the context
+        bitmap.scaleBy(x: 1.0, y: -1.0)
+        bitmap.draw(self.cgImage!, in: CGRect(x: -self.size.width / 2, y: -self.size.height / 2, width: self.size.width, height: self.size.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
 }

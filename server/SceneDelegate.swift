914568b69b68f0741e2910d387e0f5bb153b23d0
Andre Martingo <andre.martingo@gmail.com>
2021-03-06 22:40:49 +0000

    Update mac window

--- server/SceneDelegate.swift
+++ server/SceneDelegate.swift
@@ -9,10 +9,16 @@ import UIKit
 import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        UIApplication.shared.connectedScenes.compactMap { $0 as? UIWindowScene }.forEach { windowScene in
+            windowScene.sizeRestrictions?.minimumSize = CGSize(width: 400, height: 700)
+//            windowScene.sizeRestrictions?.maximumSize = CGSize(width: 400, height: 500)
+        }
+        
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: ServerView())
--- server/ServerView.swift
+++ server/ServerView.swift
@@ -13,20 +13,23 @@ struct ServerView: View {
     var body: some View {
         VStack {
             Image(uiImage: viewModel.image)
+            
             HStack {
                 Circle()
                     .fill(viewModel.connectionState == .connected ? Color.green : Color.red)
-                    .frame(width: 50, height: 50)
+                    .frame(width: 20, height: 20)
                 
                 Text(viewModel.connectionState == .connected ? "Connected" :"Connecting...")
                     .padding()
             }
         }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ServerView()
+            .previewLayout(.sizeThatFits)
     }
 }
